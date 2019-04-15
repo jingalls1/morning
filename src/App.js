66ba04775489b45dcdd38ed38796a199ab81b175
Jacob Ingalls <jingalls@uoregon.edu>
2019-04-15 01:40:41 -0700

    v1 of morning display completed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,104 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import "./styles.css";
+import { days, months, API } from "./misc.js";
+import { schedule } from "./misc.js";
 
-class App extends Component {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weatherData: "null",
+      descrip1: "",
+      descrip2: "",
+      temp: 0,
+      tempMin: 0,
+      tempMax: 0
+    };
+  }
+
+  componentWillMount() {
+    axios.get(API).then(res =>
+      this.setState({
+        weatherData: res,
+        descrip1: res.data.weather[0].main,
+        descrip2: res.data.weather[0].description,
+        temp: res.data.main.temp,
+        tempMin: Math.round(res.data.main.temp_min),
+        tempMax: Math.round(res.data.main.temp_max)
+      })
+    );
+  }
+
+  timeOfDay = () => {
+    let d = new Date();
+    if (d.getHours() < 12) {
+      return <div className="title">Good Morning, Jacob</div>;
+    } else if (d.getHours() >= 12 && d.getHours() < 6) {
+      return <div className="title">Good Afternoon, Jacob</div>;
+    } else return <div className="title">Good Evening, Jacob</div>;
+  };
+  /*
+  listLoop = () => {
+    let d = new Date().getDay();
+    let daschedule = [];
+    for (let i = 0; i < schedule[d].length; i++) {
+      daschedule.push(<li>{schedule[d][i]}</li>);
+    }
+    return daschedule;
+  };
+  */
+  listLoop = () => {
+    let d = new Date().getDay();
+    let daschedule = [];
+    for (let i = 0; i < schedule[d].length; i++) {
+      daschedule.push(<tr><td>{schedule[d][i]}</td></tr>);
+    }
+    return daschedule;
+  };
+
   render() {
+    let d = new Date();
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <br />
+        {this.timeOfDay()}
+        <br style={{ lineHeight: "1.6" }} />
+        <div className="title2"> &nbsp;Happy {days[d.getDay()]}!</div>
+        <div className="title3">
+          {" "}
+          &nbsp;{months[d.getMonth()]} {d.getDate()}, {d.getFullYear()}{" "}
+        </div>
+        <br style={{ lineHeight: "1.4" }} />
+        <div className="App">
+          <div className="weather">Here's Today's Agenda</div>
+          <table id="list">
+          {this.listLoop()}
+          </table>
+          <br />
+          <div className="weather">Weather for today in Eugene, OR</div>
+          <div className="weather2">
+            <span style={{ fontWeight: "bold" }}>
+              {this.state.descrip1} ({this.state.descrip2})
+            </span>
+          </div>
+          <div className="weather2">
+            current temp{" "}
+            <span style={{ fontWeight: "bold" }}>{this.state.temp}&deg;F</span>
+          </div>
+          <div className="weather2">
+            range{" "}
+            <span style={{ fontWeight: "bold" }}>
+              {this.state.tempMin}&deg;
+            </span>
+            &nbsp;-&nbsp;
+            <span style={{ fontWeight: "bold" }}>
+              {this.state.tempMax}&deg;F
+            </span>
+          </div>
+        </div>
       </div>
     );
   }
 }
-
-export default App;
---
+++ src/misc.js
@@ -0,0 +1,55 @@
+export const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+export const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+export const API =
+  "https://api.openweathermap.org/data/2.5/weather?zip=97007,us&APPID=8e44c500eae35929bb2690f0b5c52ac5&units=imperial";
+
+export const schedule = [
+  ["nothing today, happy sunday!"],
+  [
+    "12:00pm Italian 317",
+    "2:00pm Marketing 470",
+    "4:00pm CIT 383",
+    "5:30pm House Dinner"
+  ],
+  [
+    "8:00am International Studies 360",
+    "10:00am Duck Athletic Fund job",
+    "3:00pm Workout A/B"
+  ],
+  ["12: 00pm Italian 317", "2:00pm Marketing 470", "4:00pm CIT 383"],
+  [
+    "8:00am International Studies 360",
+    "10:00am Duck Athletic Fund job",
+    "3:00pm Workout A/B"
+  ],
+  [
+    "8:00am Duck Athletic Fund job",
+    "12:30pm Handshake Career Center Appointment",
+    "2:00pm CIT Lab",
+    "4:00pm CIT 383"
+  ],
+  ["12:00pm Workout A/B"]
+];
