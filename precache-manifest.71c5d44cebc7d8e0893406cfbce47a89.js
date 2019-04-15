689a383090ecc8aca000ac9e8771c2cb04faf799
Jacob Ingalls <jingalls@uoregon.edu>
2019-04-15 01:45:29 -0700

    Updates

---
+++ precache-manifest.71c5d44cebc7d8e0893406cfbce47a89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6823a9012e2b114ec04e",
+    "url": "/morning/static/js/runtime~main.22b1642d.js"
+  },
+  {
+    "revision": "c8c77c7187b1771710d0",
+    "url": "/morning/static/js/main.db82264c.chunk.js"
+  },
+  {
+    "revision": "69425cd1b9e2ea1ca389",
+    "url": "/morning/static/js/2.aaf6982e.chunk.js"
+  },
+  {
+    "revision": "c8c77c7187b1771710d0",
+    "url": "/morning/static/css/main.45befceb.chunk.css"
+  },
+  {
+    "revision": "fb48e81ea7ba675b323a7503a97c6643",
+    "url": "/morning/index.html"
+  }
+];
